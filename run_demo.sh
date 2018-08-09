d9872fd3c4203448d3fefa4d844f0857c8d95a59
bbaker_mrn <bbaker@mrn.org>
2018-08-09 13:25:15 -0700

    minor updates to DEMO

--- run_demo.sh
+++ run_demo.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-docker build -t gica .;
-coinstac-simulator;
-
 cd test/remote/simulatorRun;
 echo "Untarring data...";
 tar -xzf template.tar.gz;
 tar -xzf data.tar.gz;
-cd ../../../matlab_dir;
 
+cd ../../..;
+echo "Buildingg docker file...";
+docker build -t gica .;
+echo "Running Simulator...";
+coinstac-simulator;
+
+cd matlab_dir;
+echo "Displaying results...";
 matlab -nodesktop -nosplash -nodisplay -r "save_results;exit";
 
 cd ..;
